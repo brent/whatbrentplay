6b3a7d9874b02581272452572740a96d3eba29f6
brent <brent.e.meyer@gmail.com>
2018-07-03 00:28:41 -0400

    Adds firebase and config

---
+++ src/firebase/firebase.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase';
+
+var config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: "",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID 
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+const auth  = firebase.auth();
+const db    = firebase.firestore();
+
+export {
+  auth,
+  db,
+}
---
+++ src/firebase/index.js
@@ -0,0 +1,10 @@
+import * as firebase from 'firebase';
+
+import { auth } from './firebase';
+import { db } from './firebase';
+
+export {
+  firebase,
+  auth,
+  db,
+}
